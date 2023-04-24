3f760aa3ba6241ad7363fa1ff739185b42b9dfe8
melek277 <melek_yilmaz_1997@hotmail.com>
2023-04-24 15:40:26 +0300

    Initial commit

---
+++ src/Strategy/Item.java
@@ -0,0 +1,18 @@
+package Strategy;
+
+// Sepette bulunan ürünleri temsil eden sınıf.
+// Ürün kodu ve fiyat bilgileri temel olarak yeterlidir.
+public class Item {
+    private String upcCode;
+    private int price;
+
+    public Item(String upcCode, int price) {
+        this.upcCode = upcCode;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+}
+
